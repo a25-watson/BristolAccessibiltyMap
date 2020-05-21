9c1977d0de20bddf0354604719fddb95ce006aed
a25-watson <Aiden2.Watson@live.uwe.ac.uk>
2020-05-21 04:54:00 +0100

    Updates

---
+++ precache-manifest.feb6436d1c096c85bfbe2e31e2d83501.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e41827540970331cc9f6f44dfc08c6",
+    "url": "/BristolAccessebilityMap/index.html"
+  },
+  {
+    "revision": "5457c77db0b742294bdf",
+    "url": "/BristolAccessebilityMap/static/js/2.315c3a96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BristolAccessebilityMap/static/js/2.315c3a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c0f4cb3bce12f4ac54",
+    "url": "/BristolAccessebilityMap/static/js/main.22c71f5e.chunk.js"
+  },
+  {
+    "revision": "7fe1eaac5c281ed4aa45",
+    "url": "/BristolAccessebilityMap/static/js/runtime-main.9358d86c.js"
+  }
+]);
